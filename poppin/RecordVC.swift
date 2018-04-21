eb4521f7289cc49c52be44cb1dbb039c00b20a79
Tiago Martinho <martinho@thoughtworks.com>
2018-04-21 20:16:30 +0200

    Play video

--- poppin/RecordVC.swift
+++ poppin/RecordVC.swift
@@ -12,10 +12,26 @@ class RecordVC: UIViewController {
         }
         recorder.session = SCRecordSession()
         recorder.previewView = view
+        recorder.maxRecordDuration = CMTime(seconds: 3, preferredTimescale: 1)
+        recorder.delegate = self
     }
 
     @IBAction func recordTapped(_ sender: Any) {
         recorder.record()
         recordButton.isHidden = true
+    }
+}
+
+extension RecordVC: SCRecorderDelegate {
+
+    func recorder(_ recorder: SCRecorder, didComplete session: SCRecordSession) {
+        print("did complete")
+        let player = SCPlayer()
+        player.setItemBy(session.assetRepresentingSegments())
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = view.bounds
+        view.layer.addSublayer(playerLayer)
+        player.loopEnabled = true
+        player.play()
     }
 }
