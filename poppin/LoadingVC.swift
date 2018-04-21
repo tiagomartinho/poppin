89830d01a0a399117f88123443f099cecef6e70c
Tiago Martinho <martinho@thoughtworks.com>
2018-04-21 20:54:23 +0200

    Add icon

--- poppin/LoadingVC.swift
+++ poppin/LoadingVC.swift
@@ -1,5 +1,13 @@
 import UIKit
+import NVActivityIndicatorView
 
 class LoadingVC: UIViewController {
-    
+
+    @IBOutlet weak var indicatorView: NVActivityIndicatorView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        indicatorView.type = .lineScale
+        indicatorView.startAnimating()
+    }
 }
