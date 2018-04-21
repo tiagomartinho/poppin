7941ea3db8915814ce5d94f896b67431f3a8c361
Tiago Martinho <martinho@thoughtworks.com>
2018-04-21 20:45:06 +0200

    Add pops

--- Podfile
+++ Podfile
@@ -5,7 +5,7 @@ target 'poppin' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 pod 'SCRecorder'
-
+pod 'NVActivityIndicatorView'
   # Pods for poppin
 
 end
--- poppin/ViewController.swift
+++ poppin/ViewController.swift
@@ -7,9 +7,18 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var mapView: MKMapView!
     var locationManager: CLLocationManager!
 
+    let pops:[Pop] = [ Pop(title: "El Dique", imageName: "pop2", coordinate: CLLocationCoordinate2D(latitude:  41.3793779, longitude:       2.1879071)),
+                       Pop(title: "Brunch And Cake", imageName: "pop2", coordinate: CLLocationCoordinate2D(latitude: 41.3811055, longitude: 2.1871944)),
+                       Pop(title: "The Green Spot", imageName: "pop3", coordinate: CLLocationCoordinate2D(latitude: 41.3818466, longitude: 2.1836384))]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         determineCurrentLocation()
+        mapView.delegate = self
+        for pop in pops {
+            mapView?.addAnnotation(pop)
+
+        }
     }
 
     func determineCurrentLocation() {
@@ -35,3 +44,46 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         print("Error \(error)")
     }
 }
+
+
+
+class Pop: NSObject, MKAnnotation {
+    let title: String?
+    let imageName: String
+    let coordinate: CLLocationCoordinate2D
+
+    init(title: String, imageName: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.imageName = imageName
+        self.coordinate = coordinate
+
+        super.init()
+    }
+
+    var subtitle: String? {
+        return imageName
+    }
+}
+
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if let annotation = annotation as? Pop {
+            let identifier = "pin"
+            var view: MKAnnotationView
+            if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) {
+                dequeuedView.annotation = annotation
+                view = dequeuedView
+            } else {
+                view = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                view.canShowCallout = true
+                view.calloutOffset = CGPoint(x: 0, y: 5)
+                view.image = UIImage(named: annotation.imageName)
+                view.centerOffset = CGPoint(x: 0, y: -20)
+            }
+
+            return view
+        }
+        return nil
+    }
+}
+
