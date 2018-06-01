ba56599e65792585068633f2772ca93e8a2dca63
Tiago Martinho <t.martinho@icloud.com>
2018-06-01 18:03:57 +0200

    Make layer player pin to edges

--- poppin/RecordVC.swift
+++ poppin/RecordVC.swift
@@ -36,12 +36,40 @@ extension RecordVC: SCRecorderDelegate {
         player = SCPlayer()
         player.setItemBy(session.assetRepresentingSegments())
         let playerLayer = AVPlayerLayer(player: player)
-        playerLayer.frame = view.bounds
-        view.layer.addSublayer(playerLayer)
+        view.addAndPin(view: buildPlayerView(playerLayer: playerLayer))
         player.loopEnabled = true
         player.play()
         upload.isHidden = false
         view.bringSubview(toFront: upload)
         recordButton.isHidden = true
+    }
+
+    private func buildPlayerView(playerLayer: AVPlayerLayer) -> PlayerView {
+        let playerView = PlayerView()
+        playerView.playerLayer = playerLayer
+        playerLayer.videoGravity = .resizeAspectFill
+        playerView.layer.addSublayer(playerLayer)
+        return playerView
+    }
+}
+
+class PlayerView: UIView {
+
+    var playerLayer: CALayer?
+
+    override func layoutSublayers(of layer: CALayer) {
+        super.layoutSublayers(of: layer)
+        playerLayer?.frame = bounds
+    }
+}
+
+extension UIView {
+    func addAndPin(view: UIView) {
+        addSubview(view)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        view.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        view.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        view.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
     }
 }
